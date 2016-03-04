b4987a6984be57ae6e4ec4df08de26d50209f384
Frozen Helium <fren.ankit@gmail.com>
2016-03-04 20:32:13 +0545

    Add bootstrap, change directory structure

---
+++ public_html/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Locus 2016</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:100,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div id="title-block">
+        <div class="container">
+            <h1>Locus 2016</h1>
+            <div class="extra">
+                <a href="http://fb.com/locus.ioe"><i class="fa fa-facebook-official"></i>Facebook Page</a>
+                <a href="mailto:locus@ioe.edu.np"><i class="fa fa-envelope"></i>Contact Us</a>
+            </div>
+        </div>
+    </div>
+    <div id="content-block">
+        <div class="container">
+            <p id="temp-msg">Comming Soon!</p>
+        </div>
+    </div>
+</body>
+</html>
